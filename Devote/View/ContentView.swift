2f613fa82b8fa1144bd6e82974060094ccd04bc7
dotRedCoding <68290475+dotRedCoding@users.noreply.github.com>
2023-05-01 12:18:43 -0400

    - New button created for the showNewTask - has SF image for add and text of New Task - button added as a pill shape with and custom gradient to match the Content view background but travelling in another direction

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -11,6 +11,7 @@ import CoreData
 struct ContentView: View {
     // MARK: - PROPERTY
     @State var task: String = ""
+    @State private var showNewTaskItem: Bool = false
     
     // FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
@@ -23,7 +24,6 @@ struct ContentView: View {
     // MARK: - FUNCTION
     // recommended to keep functions between the properties and body
   
-    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
@@ -42,8 +42,31 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             ZStack {
+                // MARK: - MAIN VIEW
                 VStack {
                     
+                    // MARK: - HEADER
+                    Spacer(minLength: 80)
+                    
+                    // MARK: - NEW TASK BUTTON
+                    Button(action: {
+                        showNewTaskItem = true
+                    }, label: {
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 30, weight: .semibold, design: .rounded))
+                        Text("New Task")
+                            .font(.system(size: 24, weight: .bold, design: .rounded))
+                    })
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 15)
+                    .background(
+                        LinearGradient(gradient: Gradient(colors: [Color.pink, Color.blue]), startPoint: .leading, endPoint: .trailing)
+                            .clipShape(Capsule())
+                    )
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 8, x: 0.0, y: 4.0)
+                    
+                    // MARK: - TASKS
                     List {
                         ForEach(items) { item in
                             VStack(alignment: .leading) {
@@ -63,6 +86,8 @@ struct ContentView: View {
                     .padding(.vertical)
                     .frame(maxWidth: 640) // this will remove default padding and maximize it for iPad
                 } // End of VSTACK
+                
+                // MARK: - NEW TASK ITEM
             } // END of ZSTACK
             .onAppear() {
                 UITableView.appearance().backgroundColor = UIColor.clear
